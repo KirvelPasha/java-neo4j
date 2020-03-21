3120da90dedd66b69307d1b81f687f106b27fb49
KirvelPasha <kirvelpasha99@gmail.com>
2020-03-21 19:39:13 +0300

    Impl message-sender-service

---
+++ message-sender-service/src/main/java/com/demo/controller/MailSenderController.java
@@ -0,0 +1,35 @@
+package com.demo.controller;
+
+import com.demo.service.MailSenderService;
+import com.demo.wrapper.StudentDtoList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/messages")
+public class MailSenderController {
+    private final RestTemplate restTemplate;
+    private final MailSenderService mailSenderService;
+
+    @Autowired
+    public MailSenderController(RestTemplate restTemplate, MailSenderService mailSenderService) {
+        this.restTemplate = restTemplate;
+        this.mailSenderService = mailSenderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> sendMessage(@RequestParam String mail, @RequestParam Integer mark) {
+        Optional<StudentDtoList> optionalStudentDtoList = Optional
+                .ofNullable(restTemplate.getForObject("http://localhost:8081/students/filter?mark=" + mark, StudentDtoList.class));
+        mailSenderService.send(mail, optionalStudentDtoList);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ message-sender-service/src/main/java/com/demo/service/MailSenderService.java
@@ -0,0 +1,44 @@
+package com.demo.service;
+
+import com.demo.dto.StudentDto;
+import com.demo.wrapper.StudentDtoList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MailSenderService {
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public MailSenderService(@Qualifier("getJavaMailSender") JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+
+    public void send(String emailTo, Optional<StudentDtoList> optionalStudentDtoList) {
+        String subject = "MMF";
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(emailTo);
+        simpleMailMessage.setSubject(subject);
+        boolean studentsFind = false;
+        if (optionalStudentDtoList.isPresent()) {
+            List<StudentDto> studentDtoList = optionalStudentDtoList.get().getStudentDtoList();
+            if (!studentDtoList.isEmpty()) {
+                StringBuilder message = new StringBuilder();
+                studentDtoList.forEach(studentDto -> message.append(studentDto.getMail()).append('\n'));
+                simpleMailMessage.setText(message.toString());
+                studentsFind = true;
+            }
+        }
+        if (!studentsFind) {
+            simpleMailMessage.setText("Sorry");
+        }
+        javaMailSender.send(simpleMailMessage);
+    }
+}
